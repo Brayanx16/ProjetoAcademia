045155901cff8a6535069de89cdb30ee52e6d38c
Brayanx16 <brayanx16@outlook.com>
2018-06-06 20:32:49 -0300

    Arquivo de Configuração

---
+++ config/express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+module.exports = () =>{
+    return app;
+};
