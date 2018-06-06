6494b6aa473a5ad468098d49e161c8b8a7bb3e63
Marcos Neri <marcosneri@outlook.com.br>
2018-06-06 20:19:19 -0300

    erro

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-const express = require('express')
-const faker = require('faker')
-const bodyParser = require('body-parser')
-const expressLayouts = require('express-ejs-layouts')
-const app = express()
+const app = require('./config/express')();
+
 const port = process.env.PORT || 5000
 
-app.set('view engine', 'ejs')     // Setamos que nossa engine será o ejs
-app.use(expressLayouts)           // Definimos que vamos utilizar o express-ejs-layouts na nossa aplicação
-app.use(bodyParser.urlencoded())  // Com essa configuração, vamos conseguir parsear o corpo das requisições
-
-app.use(express.static(__dirname + '/public'))
 app.listen(port, () => {
     console.log(`Essa porra esta funcionando em http://localhost:${port}`)
 })
