58065023c65beb338a880f3326c64ed5041ec9e9
Brayanx16 <brayanx16@outlook.com>
2018-06-09 14:05:57 -0300

    Organizando Codigo de Rotas

--- config/express.js
+++ config/express.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs');
+app.set('views', './server/views')
 app.use(expressLayouts);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
--- server.js
+++ server.js
@@ -1,47 +1,8 @@
 const app = require('./config/express')();
-
+const rotasAdmin = require('./server/routes/admin')(app);
 const port = process.env.PORT || 3000
+
 
 app.listen(port, () => {
     console.log(`Servidor Rodando na Porta ${port}`)
-})
-
-app.get('/', (req, res) => {
-    res.render('pages/home')
-})
-
-app.get('/contact', (req, res) => {
-    res.render('pages/contact')
-})
-
-app.get('/login', (req, res) => {
-    res.render('pages/login')
-})
-
-app.get('/modalidades', (req, res) => {
-    res.render('pages/modalidades')
-})
-
-app.get('/planos', (req, res) => {
-    res.render('pages/planos')
-})
-
-app.get('/recupera-senha', (req, res) => {
-    res.render('pages/recupera-senha')
-})
-
-app.get('/cadastro-cli', (req, res) => {
-    res.render('pages/cadastro-cli')
-})
-
-app.get('/admin', (req, res) => {
-    res.render('admin/admin')
-})
-
-app.get('/user', (req, res) => {
-    res.render('cliente/user')
-})
-
-app.post('/contact', (req, res) => {
-    res.send('Obrigado por entrar em contato conosco, ' + req.body.name + '! Responderemos em breve!')
 })
