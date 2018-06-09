b2ecc2eea34a4685fc9a0a7c405acfa680ba7587
Marcos Neri <marcosneri@outlook.com.br>
2018-06-09 17:09:29 -0300

    Correção de Comentarios

--- config/express.js
+++ config/express.js
@@ -3,9 +3,9 @@ const bodyParser = require('body-parser')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
 
-app.set('view engine', 'ejs')     // Setamos que nossa engine será o ejs
-app.set('views', './server/views') //Setando basta de Views
-app.use(expressLayouts)           // Definimos que vamos utilizar o express-ejs-layouts na nossa aplicação
+app.set('view engine', 'ejs')       // Setamos que nossa engine será o ejs
+app.set('views', './server/views')  //Setando pasta de views
+app.use(expressLayouts)             // Definimos que vamos utilizar o express-ejs-layouts na nossa aplicação
 app.use(bodyParser.urlencoded({extended: true}))  // Com essa configuração, vamos conseguir parsear o corpo das requisições
 app.use(express.static('public'))
 
