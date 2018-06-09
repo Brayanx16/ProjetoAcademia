30210076a6108843b8124fb968c7079eece780da
Brayanx16 <brayanx16@outlook.com>
2018-06-09 14:16:57 -0300

    Organizando Arquivo de Rotas

--- config/express.js
+++ config/express.js
@@ -1,32 +1,14 @@
-<<<<<<< HEAD
 const express = require('express')
-const faker = require('faker')
 const bodyParser = require('body-parser')
 const expressLayouts = require('express-ejs-layouts')
 const app = express()
 
 app.set('view engine', 'ejs')     // Setamos que nossa engine será o ejs
+app.set('views', './server/views') //Setando basta de Views
 app.use(expressLayouts)           // Definimos que vamos utilizar o express-ejs-layouts na nossa aplicação
-app.use(bodyParser.urlencoded())  // Com essa configuração, vamos conseguir parsear o corpo das requisições
+app.use(bodyParser.urlencoded({extended: true}))  // Com essa configuração, vamos conseguir parsear o corpo das requisições
+app.use(express.static('public'))
 
-app.use(express.static(__dirname + 'public'))
-
-module.exports(() => {
+module.exports = () => {
      return app
-});
-=======
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const expressLayouts = require('express-ejs-layouts');
-
-app.set('view engine', 'ejs');
-app.set('views', './server/views')
-app.use(expressLayouts);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-module.exports = () =>{
-    return app;
 };
->>>>>>> 4d5ee99418815156f1758df744d3033c38e4ccf7
--- server.js
+++ server.js
@@ -1,62 +1,7 @@
 const app = require('./config/express')();
-<<<<<<< HEAD
 const rotasAdmin = require('./server/routes/admin')(app);
-=======
-
-<<<<<<< HEAD
-const port = process.env.PORT || 5000
-=======
->>>>>>> 82c26b8498f13f2477a38c42d75ae5790c1dc354
 const port = process.env.PORT || 3000
->>>>>>> 4d5ee99418815156f1758df744d3033c38e4ccf7
-
 
 app.listen(port, () => {
-<<<<<<< HEAD
     console.log(`Servidor Rodando na Porta ${port}`)
-=======
-    console.log(`Servidor Rodando na Porta http://localhost:${port}`)
-})
-
-app.get('/', (req, res) => {
-    res.render('pages/home')
-})
-
-app.get('/contact', (req, res) => {
-    res.render('pages/contact')
-})
-
-app.get('/login', (req, res) => {
-    res.render('pages/login')
-})
-
-app.get('/modalidades', (req, res) => {
-    res.render('pages/modalidades')
-})
-
-app.get('/planos', (req, res) => {
-    res.render('pages/planos')
-})
-
-app.get('/recupera-senha', (req, res) => {
-    res.render('pages/recupera-senha')
-})
-
-app.get('/cadastro-cli', (req, res) => {
-    res.render('pages/cadastro-cli')
-})
-
-app.get('/admin', (req, res) => {
-    res.render('admin/admin')
-})
-
-app.get('/user', (req, res) => {
-    res.render('cliente/user')
-})
-app.get('/about', (req, res) => {
-    res.render('pages/about')
-})
-app.post('/contact', (req, res) => {
-    res.send('Obrigado por entrar em contato conosco, ' + req.body.name + '! Responderemos em breve!')
->>>>>>> 82c26b8498f13f2477a38c42d75ae5790c1dc354
-})
+});
