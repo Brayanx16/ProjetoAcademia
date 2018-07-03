8682154c5bb2d5719e78ef94918fdab99f4d4822
Brayanx16 <brayanx16@outlook.com>
2018-07-03 20:06:50 -0300

    correnção de bugs e implementação do Relatorio

--- app/controllers/routesController.js
+++ app/controllers/routesController.js
@@ -96,19 +96,55 @@ controller.deleteUsuario = (req, res) => {
   });
 };
 
-controller.listCliRelatorios = (req, res) => {
+/*controller.listCliRelatorios = (req, res) => {
   req.getConnection((erro, conn) => {
     conn.query('select * from cliente', (erros, admin) => {
+      let lista = [];
+
+      for(i = 0; i < admin.length; i++){
+        let obj = {nome: '', idade: '', rg: '', cpf: '', email: '', telefone: '', sexo: '', cep: '', 
+        estado: '', cidade: '', bairro: '', senha: '', plano: '', dataC: '', dataR: '', dataN: ''}
+          
+        let dataFormatada = '0'+admin[i].dataR.getDate() + '/' + '0'+(admin[i].dataR.getMonth() + 1) + '/' + admin[i].dataR.getFullYear()
+
+        obj.nome = admin[i].nome
+        obj.idade = admin[i].idade
+        obj.rg = admin[i].rg
+        obj.cpf = admin[i].cpf
+        obj.email = admin[i].email
+        obj.telefone = admin[i].telefone
+        obj.sexo = admin[i].sexo
+        obj.cep = admin[i].cep
+        obj.estado = admin[i].estado
+        obj.cidade = admin[i].cidade
+        obj.bairro = admin[i].bairro
+        obj.senha = admin[i].senha
+        obj.plano = admin[i].plano
+        obj.dataC = admin[i].dataC
+        obj.dataR = dataFormatada
+        obj.dataN = admin[i].dataN
+
+        lista.push(obj)
+      }
+      //res.send(lista);
+      res.render('relatorios/listCli', {data: lista});
+    });
+  });
+};*/
+
+controller.listCliRelatorios = (req, res) => {
+  req.getConnection((erro, conn) => {
+    conn.query('select nome, idade, rg, cpf, email, telefone, sexo, cep, estado, cidade, bairro, senha, plano, dataC, date_format(dataR, "%d/%m/%Y") as dataR , dataN from cliente', (erros, admin) => {
+      
       res.render('relatorios/listCli', {data: admin});
     });
   });
 };
 
-
-
 // Rodas do Cliente
 controller.listCliente = (req, res) => {
   req.getConnection((erro, conn) => {
+    
     conn.query('select * from cliente', (erros, admin) => {
       res.render('cliente/listCli', {data: admin});
     });
@@ -117,12 +153,13 @@ controller.listCliente = (req, res) => {
 
 controller.addCliente = (req, res) => {
   const data = req.body;
-  console.log(data);
-  
+    
   req.getConnection((err, connection) => {
-    const query = connection.query('insert into cliente set ?', data, (erros, customer) => {
-      res.redirect('/cliente/listCli');
-    });
+    const query = connection.query('insert into cliente (nome, idade, rg, cpf, email, telefone, sexo, cep, estado, cidade, bairro, senha, plano, dataC, dataR, dataN) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURDATE(), CURDATE(), ?)'
+    , [data.nome, data.idade, data.rg, data.cpf, data.email, data.telefone, data.sexo, data.cep, data.estado, data.cidade, data.bairro, data.senha, data.plano, data.dataN] , (err, customer) => {
+      
+     res.redirect('/cliente/listCli');
+   });
   });
 };
 
--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -1,4 +1,3 @@
-
 $('[data-toggle="tooltip"]').tooltip();
 
 function verificaNumero(e) {
@@ -37,41 +36,73 @@ function FormataCEP(evt) {
     return vr;
 }
 
-function formataData(data = new Date()){
-    var dia = data.getDate();
-    var mes = data.getMonth()+1;
-    var ano = data.getFullYear();
-
-    if(dia.toString().length == 1){
-        dia = '0'+dia;
-    }if(mes.toString().length == 1){
-        mes = '0'+mes;
+function mascaraData(val) {
+    var pass = val.value;
+    var expr = /[0123456789]/;
+  
+    for (i = 0; i < pass.length; i++) {
+      // charAt -> retorna o caractere posicionado no índice especificado
+      var lchar = val.value.charAt(i);
+      var nchar = val.value.charAt(i + 1);
+  
+      if (i == 0) {
+        // search -> retorna um valor inteiro, indicando a posição do inicio da primeira
+        // ocorrência de expReg dentro de instStr. Se nenhuma ocorrencia for encontrada o método retornara -1
+        // instStr.search(expReg);
+        if ((lchar.search(expr) != 0) || (lchar > 3)) {
+          val.value = "";
+        }
+  
+      } else if (i == 1) {
+  
+        if (lchar.search(expr) != 0) {
+          // substring(indice1,indice2)
+          // indice1, indice2 -> será usado para delimitar a string
+          var tst1 = val.value.substring(0, (i));
+          val.value = tst1;
+          continue;
+        }
+  
+        if ((nchar != '/') && (nchar != '')) {
+          var tst1 = val.value.substring(0, (i) + 1);
+  
+          if (nchar.search(expr) != 0)
+            var tst2 = val.value.substring(i + 2, pass.length);
+          else
+            var tst2 = val.value.substring(i + 1, pass.length);
+  
+          val.value = tst1 + '/' + tst2;
+        }
+  
+      } else if (i == 4) {
+  
+        if (lchar.search(expr) != 0) {
+          var tst1 = val.value.substring(0, (i));
+          val.value = tst1;
+          continue;
+        }
+  
+        if ((nchar != '/') && (nchar != '')) {
+          var tst1 = val.value.substring(0, (i) + 1);
+  
+          if (nchar.search(expr) != 0)
+            var tst2 = val.value.substring(i + 2, pass.length);
+          else
+            var tst2 = val.value.substring(i + 1, pass.length);
+  
+          val.value = tst1 + '/' + tst2;
+        }
+      }
+  
+      if (i >= 6) {
+        if (lchar.search(expr) != 0) {
+          var tst1 = val.value.substring(0, (i));
+          val.value = tst1;
+        }
+      }
     }
-
-    return dia+'/'+mes+'/'+ano;
-}
-
-    console.log(document.querySelector("#pagamento").textContent);
-
-
-/*$(document).ready(function () {
-    $('.ocultarfundo').on('click', function () {
-        $('.barra').toggleClass("barraoculta"); //classe onde a barra fica transparente, ou de outra forma.
-    });
-});
-$(window).scroll(function () {
-
-    if ($(this).scrollTop() > 10) {
-        $('.navbar').fadeOut();
-    }
-    else {
-        $('.navbar').fadeIn();
-    }
-});*/
-
-/*$(document).ready(function () {
-    $('.button-left').click(function () {
-        $('.sidebar').toggleClass('fliph');
-    });
-
-});*/
+  
+    if (pass.length > 10)
+      val.value = val.value.substring(0, 10);
+    return true;
+  }
